9799e884321a9819e0050249abc8a998272b5176
UltraFutile <ultrafutile@protonmail.com>
2021-07-25 11:57:34 -0500

    Configured basic postgres connection, added user entity

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-	imports: [],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: 'localhost',
+			port: 5432,
+			username: 'metricsadmin', //custom localhost test user I created.
+			password: 'metrics',
+			database: 'metricsdb',
+			entities: [User],
+			synchronize: true,
+		}),
+		UsersModule
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
